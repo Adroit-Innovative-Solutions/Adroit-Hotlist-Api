679bc443df66d556feceb28111e8adbef571f332
SasiKDP <sasank9110@gmail.com>
2025-07-04 10:32:17 +0530

    git intialized

---
+++ src/main/java/com/adroit/hotlistmicroservice/HotlistmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adroit.hotlistmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotlistmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotlistmicroserviceApplication.class, args);
+	}
+
+}
